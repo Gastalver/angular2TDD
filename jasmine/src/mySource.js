cd583c298fd23745763f39e8cbaf51dd64fe2c85
Gastalver <mgastalver@gmail.com>
2017-11-16 19:53:54 +0100

    Cap.2

---
+++ jasmine/src/mySource.js
@@ -0,0 +1,11 @@
+var a,
+    myObject={
+    setA: function(value){
+        "use strict";
+        a = value;
+    },
+    getA: function(value){
+        "use strict";
+        return a;
+    }
+    }
